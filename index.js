dae89964ba1669264cf9e77680f09b57a4fc2852
Peter Pritchard <ppritchard@demandware.com>
2015-07-13 10:52:04 -0400

    progress check-in fo Lare

--- index.js
+++ index.js
@@ -12,6 +12,190 @@ var path = require('path');
 var exec = require('child_process').exec;
 var spawn = require('child_process').spawn;
 
+function GitService( config ) {
+  this.sourceDir = config.sourceDir || './tmp/engine_db';
+  this.remote = {
+    name: config.name || 'origin',
+    url: config.url,
+    branch: config.branch || 'master'
+  };
+}
+
+GitService.prototype.init = function(){
+  var self = this;
+  return new Promise(function(resolve, reject){
+    push( self.sourceDir, self.remote, function(){
+      resolve();
+    })
+  });
+};
+
+  // clone latest from remote (git clone <remote> <branch> <repo>)
+ 
+  // 0. create directories (if necessary) 
+  // 1. git clone <remote> <branch> <sourceDir>
+GitService.prototype.clone = function(){
+  var self = this;
+  return new Promise(function(resolve, reject){
+    clone( self.sourceDir, self.remote, function(){
+      resolve();
+    })
+  });
+},
+
+  // pull the latest from remote (git pull <remote> <branch> <repo>)
+
+  // 0. if directories don't exist, then clone instead
+  // 1. otherwise, git pull <remote> <branch>
+GitService.prototype.pull = function(){
+  return new Promise(function(resolve, reject){
+    reject(new Error('unimplemented!'));
+  });
+},
+
+  // push new files to repo
+
+  // 0. create directories (if necessary) 
+  // 1. git init (unless already a git repo)
+  // 2. set up remote (unless already exists)
+  // 3. set up branch (unless already exists)
+  // 4. call git push <remote> <branch> (if new)
+  // 5. fetch remote repo
+  // 6. ... write bootstrap files for tenant type ... (if new)
+  // 7. add all new files
+  // 8. commit -m 'Initial import'
+  // 9. git push <remote> <branch>
+GitService.prototype.push = function(){
+  var self = this;
+  return new Promise(function(resolve, reject){
+    push( self.sourceDir, self.remote, function(){
+      resolve();
+    })
+  });
+};
+
+// Internal API
+function exists(organization_id, repo_name, cb) {
+  cb = cb || function() {};
+
+  if(!organization_id || !repo_name ) {
+    throw new Error('need org id & repo name to test for existence');
+  }
+
+  var options = {cwd: sourceDir, stdio: 'inherit'};
+  var message = 'Created ' + new Date().toISOString();
+
+  // Start with an empty promise
+  return new Promise(function(resolve, reject) {
+
+    spawn('curl', ['-X', 'GET', 
+      'https://api.github.com/repos/'+organization_id+'/'+repo_name],
+      {cwd:'.',stdout:'inherit'})
+    .on('data', function(data){
+      console.log(data);
+    })
+    .on('exit', function(code){
+      if(code === 0) {
+        resolve(true);
+        cb(null, true);
+      } else {
+        reject(false);
+        cb(null, false);
+      }
+    })
+
+  });
+
+}
+
+function deleteRemoteRepo(organization_id, repo_name, access_token, cb) {
+  cb = cb || function() {};
+
+  if(!organization_id || !repo_name || !access_token ) {
+    throw new Error('need org id, repo name and access token to delete a repo');
+  }
+
+  var options = {cwd: '.', stdio: 'inherit'};
+  var message = 'Deleted ' + new Date().toISOString();
+
+  // Start with an empty promise
+  return new Promise(function(resolve, reject) {
+
+    spawn('curl', ['-X', 'DELETE', 
+      'https://api.github.com/repos/'+organization_id+'/'+repo_name,
+      '--header', 'Authorization: token '+ access_token],
+      {cwd:'.',stdout:'inherit'})
+    .on('exit', function(code){
+      if(code === 0) {
+        resolve();
+        cb();
+      } else {
+        reject(new Error('could not delete repo: '+organization_id+'/'+repo_name));
+      }
+    })
+
+  });
+
+}
+
+function createRemoteRepo(organization_id, repo_name, access_token, cb) {
+  cb = cb || function() {};
+
+  if(!organization_id || !repo_name || !access_token ) {
+    throw new Error();
+  }
+
+  var options = {cwd: '.', stdio: 'inherit'};
+  var message = 'Created ' + new Date().toISOString();
+
+  // Start with an empty promise
+  return new Promise(function(resolve, reject) {
+
+    spawn('curl', ['-X', 'POST', 
+      '--data', '{"name": "'+repo_name+'","description": "This is a test repository","homepage": "https://github.com/OpenCommerce","private": false,"has_issues": true,"has_wiki": true,"has_downloads": true}', 
+      'https://api.github.com/orgs/'+organization_id+'/repos', 
+      '--header', 'Content-Type: application/json', 
+      '--header', 'Authorization: token '+ access_token],
+      {cwd:'.',stdout:'inherit'})
+    .on('exit', function(code){
+      if(code === 0) {
+        resolve();
+        cb();
+      } else {
+        reject('Failed to create git repo: '+organization_id+'/'+repo_name);
+      }
+    })
+
+  });
+}
+
+function ensureWorkingFolderExists(sourceDir){
+  return new Promise(function(resolve, reject) {
+
+    fs.exists(sourceDir, function(err, exists){
+      if( err ) {
+        reject( err );
+      } else {
+        if( !exists ) {
+          console.log('creating working folder');
+          fs.mkdir(sourceDir, function(err){
+            if( err ) {
+              reject( err );
+            } else {
+              resolve( true );
+            }
+          });
+        } else {
+          resolve(false);
+        }
+        resolve( exists );
+      }
+    });
+
+  });
+}
+
+
 function push(sourceDir, remote, cb) {
 
   if (!path.isAbsolute(sourceDir) && process) {
@@ -31,13 +215,7 @@ function push(sourceDir, remote, cb) {
   var message = 'Update ' + new Date().toISOString();
 
   // Start with an empty promise
-  Promise.resolve()
-
-    //
-    // Initialize a new Git repository if it doesn't exist
-    // -------------------------------------------------------------------------
-    .then(function() {
-      return new Promise(function(resolve, reject) {
+  return new Promise(function(resolve, reject) {
         if (!fs.existsSync(path.join(options.cwd, '.git'))) {
           spawn('git', ['init'], options)
             .on('exit', function(code) {
@@ -92,7 +270,7 @@ function push(sourceDir, remote, cb) {
                 .on('exit', function() {
                   spawn('git', ['commit', '-m', message], options)
                     .on('exit', function() {
-                      spawn('git', ['push', remote.name, 'master'], options)
+                      spawn('git', ['push', remote.name, remote.branch], options)
                         .on('exit', function() {
                           cb();
                         });
@@ -180,7 +358,7 @@ function push(sourceDir, remote, cb) {
     .then(function() {
       return new Promise(function(resolve, reject) {
         console.log('Pushing to ' + remote.url);
-        spawn('git', ['push', remote.name, 'master'], options)
+        spawn('git', ['push', remote.name, remote.branch], options)
           .on('exit', function(code) {
             if (code === 0) {
               cb();
@@ -199,4 +377,68 @@ function push(sourceDir, remote, cb) {
     });
 }
 
-module.exports = push;
+function clone(sourceDir, remote, cb) {
+
+  if (!path.isAbsolute(sourceDir) && process) {
+    sourceDir = path.join(process.cwd(), sourceDir);
+  }
+
+  if (typeof remote === 'string') {
+    remote = {name: 'origin', url: remote, branch: 'master'};
+  }
+
+  remote.branch = remote.branch || 'master';
+  remote.name = remote.name || 'origin';
+
+  cb = cb || function() {};
+
+  var options = {cwd: sourceDir, stdio: 'inherit'};
+  var message = 'Update ' + new Date().toISOString();
+
+  // Start with an empty promise
+  Promise.resolve()
+
+    //
+    // Initialize a new place to work if it doesn't exist
+    // -------------------------------------------------------------------------
+    .then(function(){
+      return createWorkingFolder( sourceDir );
+    })
+   //
+    // Initialize a new Git repository if it doesn't exist
+    // -------------------------------------------------------------------------
+    .then(function(created) {
+      return new Promise(function(resolve, reject) {
+        if (created) {
+          console.log('cloning latest into '+options.cwd);
+          spawn('git', ['clone', remote.url, '.'], options)
+            .on('exit', function(code) {
+              if (code === 0) {
+                resolve();
+              } else {
+                reject('Failed to clone an existing Git repository.');
+              }
+            });
+        } else {
+          console.log('pulling latest into '+options.cwd);
+          spawn('git', ['pull', remote.name, remote.branch], options)
+            .on('exit', function(code) {
+              if (code === 0) {
+                resolve();
+              } else {
+                reject('Failed to pull the latest existing Git repository.');
+              }
+            });
+        }
+      });
+    })
+
+    //
+    // Catch errors
+    // -------------------------------------------------------------------------
+    .catch(function(err) {
+      cb(err || 'Failed to clone/pull the latest contents.');
+    });
+}
+
+module.exports = GitService;
--- test.js
+++ test.js
@@ -1,12 +1,86 @@
 var path = require('path');
-var push = require('./index.js');
+var spawn = require('child_process').spawn;
 
-var remote = {
-  name: 'test',
-  url: 'https://github.com/koistya/test.git',
-  branch: 'master'
-};
+// var GitSvc = require('./index.js');
+// var gitSvc1 = new GitSvc({
+//   name: 'origin',
+//   url: 'https://github.com/OpenCommerce/test-domain-db3.git',
+//   branch: 'master',
+//   sourceDir: './tmp/test-domain-db'
+// });
 
-push('./test', remote, function() {
-  console.log('Done!');
+// var gitSvc2 = new GitSvc({
+//   name: 'origin',
+//   url: 'https://github.com/OpenCommerce/test-domain-db.git',
+//   branch: 'master',
+//   sourceDir: './tmp/test-domain-db2'
+//});
+
+//gitSvc1.push()
+// .then(function(){
+// 	console.log('finished push to test-domain-db');
+
+// 	return gitSvc2.clone();
+// })
+// .then(function(){
+// 	console.log('finished clone to test-domain-db');
+// })
+// .catch(function(err){
+// 	console.error('failed to complete test');
+// 	console.error(err);
+// });
+
+
+// var createProc = spawn('curl', ['-X', 'DELETE', 
+// 	'https://api.github.com/repos/OpenCommerce/test-domain-db5', 
+// 	'--header', 'Authorization: token <insert token here>'],
+// 	{cwd:'.',stdout:'inherit'});
+// curl 
+// -X 
+// DELETE 
+// https://api.github.com/repos/OpenCommerce/test-domain-db6 
+// -H
+// "Authorization: token <insert token here>"
+
+var createProc = spawn('curl', ['-X', 'POST', 
+	'--data', '{"name": "test-domain-db5","description": "This is a test repository","homepage": "https://github.com/OpenCommerce","private": false,"has_issues": true,"has_wiki": true,"has_downloads": true}', 
+	'https://api.github.com/orgs/OpenCommerce/repos', 
+	'--header', 'Content-Type: application/json', 
+	'--header', 'Authorization: token <insert token here>'],
+	{cwd:'.',stdout:'inherit'});
+
+createProc.on('exit', function(code) {
+  console.log(code);
 });
+
+createProc.stdout.on('data', function (data) {
+  console.log('stdout: ' + data);
+});
+
+createProc.stderr.on('data', function (data) {
+  console.log('stderr: ' + data);
+});
+
+createProc.on('close', function (code) {
+  console.log('child process exited with code ' + code);
+});
+
+// spawn('git', ['clone', 'https://github.com/OpenCommerce/test-domain-db.git', './tmp/test-domain-db3'], {cwd:'.',stdout:'inherit'})
+//   .on('exit', function(code) {
+//     console.log(code);
+//   });
+
+// curl 
+// -X POST 
+// -d '{"name": "test-domain-db6",
+// "description": "This is a test repository",
+// "homepage": "https://github.com/OpenCommerce",
+// "private": false,
+// "has_issues": true,
+// "has_wiki": true,
+// "has_downloads": true
+// }' 
+// https://api.github.com/orgs/OpenCommerce/repos 
+// -H "Content-Type: application/json"
+//-H "Authorization: token <insert token here>"
+// curl -X POST -d '{"name": "test-domain-db5","description": "This is a test repository","homepage": "https://github.com/OpenCommerce","private": false,"has_issues": true,"has_wiki": true,"has_downloads": true}' https://api.github.com/orgs/OpenCommerce/repos -H "Content-Type: application/json" -H "Authorization: token fb90466db7c8d3db72e7335a0952f2e5df0d4fd2"
